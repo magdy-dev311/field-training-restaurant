785c7f9cf028e419282e26b70fc81741dc72e124
magdyZiad <magdyziad.dev@gmail.com>
2025-12-12 19:04:08 +0200

    upload my changes

---
+++ main.js
@@ -0,0 +1,18 @@
+let menu = document.getElementById("menu");
+let list = document.getElementById("list");
+
+menu.onclick = () => {
+  list.classList.toggle("list");
+};
+
+links = document.querySelectorAll(".min-link");
+links.forEach((x) => {
+  x.onclick = () => {
+    list.classList.toggle("list");
+  };
+});
+
+// let hreo = document.getElementById("hero");
+// hreo.onclick = () => {
+//   list.classList.toggle("list");
+// };
